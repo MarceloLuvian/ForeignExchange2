3c9fa30e46d1945bdafa90876d116018d05a2f38
MarceloLuvian <luvianmarcelo@gmail.com>
2018-01-29 13:57:18 -0600

    Helpers, Interfaces y Resources para que la aplicacion use el lenguaje que se configura en el dispositivo.

---
+++ ForeignExchange2/ForeignExchange2/Interfaces/ILocalize.cs
@@ -0,0 +1,10 @@
+﻿namespace ForeignExchange2.Interfaces
+{
+    using System.Globalization;
+
+    public interface ILocalize
+     {
+        CultureInfo GetCurrentCultureInfo();
+        void SetLocale(CultureInfo ci);
+     }
+}
