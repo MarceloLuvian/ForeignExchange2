0351d3d934ae40556acfa26fd04374ae89ff1796
MarceloLuvian <luvianmarcelo@gmail.com>
2018-01-30 22:36:11 -0600

    Agredala la opcion multilenguaje en el proyecto Agregada la clase ApiServices para el consumo de servicios WEB

---
+++ ForeignExchange2/ForeignExchange2/ApiService.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+using ForeignExchange2.Models;
+using Newtonsoft.Json;
+
+namespace ForeignExchange2
+{
+    public class ApiService
+    {
+        //public async Response CheckConnection()
+        //{
+            
+        //}
+
+        public async Task<Response> GetList<T>(string urlBase, string controller)
+        {
+
+            try
+            {
+                var client = new HttpClient();
+                client.BaseAddress = new Uri(urlBase);
+                var response = await client.GetAsync(controller);
+                var result = await response.Content.ReadAsStringAsync();
+                if (!response.IsSuccessStatusCode)
+                {
+                    return new Response
+                    {
+                        IsSuccess = false,
+                        Message = result,
+                    };
+                }
+
+                var list = JsonConvert.DeserializeObject<List<T>>(result);
+
+                return new Response
+                {
+                    IsSuccess = true,
+                    Result = list,
+                };
+            }
+            catch (Exception ex)
+            {
+                return new Response
+                {
+                    IsSuccess = false,
+                    Message = ex.Message,
+                };
+            }
+        }
+    }
+}
--- ForeignExchange2/ForeignExchange2/Helpers/Lenguages.cs
+++ ForeignExchange2/ForeignExchange2/Helpers/Lenguages.cs
@@ -12,10 +12,6 @@
             Resource.Culture = ci;
             DependencyService.Get<ILocalize>().SetLocale(ci);
         }
-        public static string Title
-        {
-            get { return Resource.Title; }
-        }
         public static string Accept
         {
             get { return Resource.Accept; }
@@ -28,5 +24,9 @@
         {
             get { return Resource.Error; }
         }
+        public static string Title
+        {
+            get { return Resource.Title; }
+        }     
     } 
 }
